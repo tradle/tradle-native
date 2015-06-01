b3404116b4bc2d513e31bc1aae0889c9e7f21d0d
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-06-01 13:58:11 -0400

    net, fs, browser field hacks

--- fixpkgs.js
+++ fixpkgs.js
@@ -6,7 +6,6 @@ var fs = require('fs')
 var find = require('findit')
 var path = require('path');
 var thisPkg = require('./package.json')
-var allDeps = {}
 
 // function loadDeps() {
 //   var pkgs = []
@@ -28,66 +27,116 @@ var allDeps = {}
 // loadDeps(hackFiles)
 
 var hackers = [
+  // {
+  //   name: 'fssync',
+  //   regex: [
+  //     /webtorrent\/lib\/fs-storage\.js/
+  //   ],
+  //   hack: function(file, contents) {
+  //     contents = contents.toString()
+  //     var fixed = contents.replace(/fs\.existsSync\([^\)]*\)/g, 'false')
+  //     return contents === fixed ? null : fixed
+  //   }
+  // },
   {
-    name: 'fssync',
+    name: 'browser_field',
     regex: [
-      /webtorrent\/lib\/fs-storage\.js/
+      /package\.json$/
     ],
-    handler: function(file, contents) {
-      contents = contents.toString()
-      var fixed = contents.replace(/fs\.existsSync\([^\)]*\)/g, 'false')
-      return contents === fixed ? null : fixed
+    hack: function(file, contents) {
+      var pkg
+      try {
+        pkg = JSON.parse(contents)
+      } catch (err) {
+        console.log('failed to parse:', file)
+        return
+      }
+
+      if (pkg.browser && typeof pkg.browser === 'object') {
+        var fixed
+        for (var left in pkg.browser) {
+          if (left[0] === '.' && !/\.[a-z]{0,4}$/i.test(left)) {
+            fixed = true
+            pkg.browser[left + '.js'] = pkg.browser[left]
+            delete pkg.browser[left]
+          }
+        }
+
+        if (fixed) return JSON.stringify(pkg, null, 2)
+      }
     }
   },
+  // {
+  //   name: 'version',
+  //   regex: [
+  //     /\.js$/
+  //   ],
+  //   hack: function(file, contents) {
+  //     var fixed = contents
+  //     var matchPkg;
+  //     do {
+  //       matchPkg = fixed.match(/require\('?"?([^'"\)]*?package\.json)'?"?\)/)
+  //       if (matchPkg) {
+  //         var pkgPath = path.join(path.dirname(file), matchPkg[1])
+  //         var pkg
+  //         try {
+  //           pkg = require(pkgPath)
+  //         } catch (err) {
+  //           console.warn('failed to hack', file)
+  //           break;
+  //         }
+
+  //         fixed = fixed.replace(matchPkg[0], JSON.stringify(pkg))
+  //       }
+  //     } while (matchPkg)
+
+  //     return fixed
+  //   }
+  // },
   {
-    name: 'version',
+    name: 'net',
     regex: [
-      /\.js$/
+      /bittorrent-swarm\/package\.json$/,
+      /portfinder\/package\.json$/
     ],
-    handler: function(file, contents) {
-      contents = contents.toString()
+    hack: function(file, contents) {
+      var pkg
+      try {
+        pkg = JSON.parse(contents)
+      } catch (err) {
+        console.log('failed to parse:', file)
+        return
+      }
 
-      var fixed = contents
-      var matchPkg;
-      do {
-        matchPkg = fixed.match(/require\('?"?([^'"\)]*?package\.json)'?"?\)/)
-        if (matchPkg) {
-          var pkgPath = path.join(path.dirname(file), matchPkg[1])
-          var pkg
-          try {
-            pkg = require(pkgPath)
-          } catch (err) {
-            console.warn('failed to hack', file)
-            break;
-          }
-
-          fixed = fixed.replace(matchPkg[0], JSON.stringify(pkg))
-        }
-      } while (matchPkg)
-
-      return contents === fixed ? null : fixed
+      rewireMain(pkg)
+      if (pkg.browser.net !== 'utp') {
+        pkg.browser.net = 'utp'
+        return JSON.stringify(pkg, null, 2)
+      }
     }
   },
   {
     name: 'fs',
     regex: [
-      /bitkeeper-js\/package\.json/,
-      /glob\/package\.json/,
-      /random-access-file\/package\.json/,
-      /pump\/package\.json/,
-      /mime\/package\.json/,
-      /walk\/package\.json/,
-      /rimraf\/package\.json/,
-      /webtorrent\/package\.json/,
-      /formidable\/package\.json/,
-      /mkdirp\/package\.json/,
-      /tradle-utils\/package\.json/,
-      /create-torrent\/package\.json/,
-      /form-data\/package\.json/,
-      /blockloader\/package\.json/,
-      /chained-obj\/package\.json/
+      /bitkeeper-js\/package\.json$/,
+      /glob\/package\.json$/,
+      /random-access-file\/package\.json$/,
+      /pump\/package\.json$/,
+      /mime\/package\.json$/,
+      /walk\/package\.json$/,
+      /rimraf\/package\.json$/,
+      /webtorrent\/package\.json$/,
+      /formidable\/package\.json$/,
+      /mkdirp\/package\.json$/,
+      /load-ip-set\/package\.json$/,
+      /portfinder\/package\.json$/,
+      /tradle-utils\/package\.json$/,
+      /create-torrent\/package\.json$/,
+      /form-data\/package\.json$/,
+      /blockloader\/package\.json$/,
+      /chained-obj\/package\.json$/
     ],
-    handler: function(file, contents) {
+    hack: function(file, contents) {
       var pkg
       try {
         pkg = JSON.parse(contents)
@@ -106,13 +155,13 @@ var hackers = [
   {
     name: 'dgram',
     regex: [
-      /sock-jack\/package\.json/,
-      /utp\/package\.json/,
-      /bittorrent-dht\/package\.json/,
-      /zlorp\/package\.json/,
-      /dns\.js\/package\.json/
+      /sock-jack\/package\.json$/,
+      /utp\/package\.json$/,
+      /bittorrent-dht\/package\.json$/,
+      /zlorp\/package\.json$/,
+      /dns\.js\/package\.json$/
     ],
-    handler: function(file, contents) {
+    hack: function(file, contents) {
       var pkg
       try {
         pkg = JSON.parse(contents)
@@ -131,11 +180,10 @@ var hackers = [
   {
     name: 'simpleget',
     regex: [
-      /simple\-get\/index\.js/
+      /simple\-get\/index\.js$/
     ],
-    handler: function(file, contents) {
+    hack: function(file, contents) {
       var hack = ";res.headers = res.headers || {};"
-      contents = contents.toString()
       if (contents.indexOf(hack) !== -1) return
 
       return contents.replace(
@@ -151,39 +199,57 @@ function hackFiles() {
   var finder = find('./node_modules')
 
   finder.on('file', function (file) {
-    if (!/\.js$/.test(file)
-      || /\/test\//.test(file)) return
+    if (!/\.(js|json)$/.test(file)
+      || /\/tests?\//.test(file)) return
 
-    var parts = file.split('/')
-    var idx = 0
-    // var idx = parts.indexOf(path.basename(__dirname))
-    while ((idx = parts.indexOf('node_modules', idx)) !== -1) {
-      var dep = parts[idx + 1]
-      var parentPkgPath = idx === 0 ? './package.json' :
-        path.join(parts.slice(0, idx).join('/'), 'package.json')
-      parentPkgPath = path.resolve(parentPkgPath)
-      var parentPkg = require(parentPkgPath)
-      if (!(dep in parentPkg.dependencies)) return
+    // var parts = file.split('/')
+    // var idx = 0
+    // // var idx = parts.indexOf(path.basename(__dirname))
+    // while ((idx = parts.indexOf('node_modules', idx)) !== -1) {
+    //   var dep = parts[idx + 1]
+    //   var parentPkgPath = idx === 0 ? './package.json' :
+    //     path.join(parts.slice(0, idx).join('/'), 'package.json')
+    //   parentPkgPath = path.resolve(parentPkgPath)
+    //   var parentPkg = require(parentPkgPath)
+    //   if (!(dep in parentPkg.dependencies)) return
 
-      parts = parts.slice(idx + 2)
-    }
+    //   parts.unshift() // node_modules
+    //   parts.unshift() // dep
+    // }
 
-    hackers.some(function(hacker) {
-      if (hacker.regex.some(function(regex) { return regex.test(file) })) {
-        file = path.resolve(file)
-        fs.readFile(file, function(err, buf) {
-          if (err) throw err
-
-          var hacked = hacker.handler(file, buf)
-          if (hacked) {
-            console.log('hacking', file)
-            fs.writeFile(file, hacked)
-          }
-        })
-
-        return true
-      }
+    var matchingHackers = hackers.filter(function(hacker) {
+      return hacker.regex.some(function(regex) {
+        return regex.test(file)
+      })
     })
+
+    if (!matchingHackers.length) return
+
+    file = path.resolve(file)
+    // if (/\.json$/.test(file)) {
+    //   try {
+    //     var json = JSON.parse(require(file))
+    //     onread(null, json)
+    //   } catch (err) {
+    //     console.warn('failed to parse:', file)
+    //   }
+    // }
+    // else {
+      fs.readFile(file, { encoding: 'utf8' }, onread)
+    // }
+
+    function onread(err, str) {
+      if (err) throw err
+
+      var hacked = matchingHackers.reduce(function(hacked, hacker) {
+        return hacker.hack(file, hacked || str) || hacked
+      }, str)
+
+      if (hacked && hacked !== str) {
+        console.log('hacking', file)
+        fs.writeFile(file, hacked)
+      }
+    }
   });
 }
 
